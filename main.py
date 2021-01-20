830ac7c9fcd8297a6877909fe2fd2968c35c27ee
Szermekm <matyasszermek@gmail.com>
2021-01-20 19:06:09 +0100

    Final solution w 3 types of Fish

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ class Aquarium:
     def __init__(self):
         self.fish = []
 
-    def addFish(self, fish):
+    def addfish(self, fish):
         self.fish.append(fish)
 
     def feed(self):
         for i in self.fish:
             i.feed()
 
-    def removeFish(self):
+    def removefish(self):
         for number, i in enumerate(self.fish):
             if i.weight > 10:
                 self.fish.pop(number)
 
-    def getStatus(self):
+    def getstatus(self):
         for i in self.fish:
             i.status()
 
@@ -27,9 +27,9 @@ class Fish:
 
 
 class Clownfish(Fish):
-    def __init__(self, name, weight, color, stripeColor):
+    def __init__(self, name, weight, color, stripecolor):
         super(Clownfish, self).__init__(name, weight, color)
-        self.stripeColor = stripeColor
+        self.stripecolor = stripecolor
 
     def feed(self):
         self.weight = self.weight + 1
@@ -38,10 +38,32 @@ class Clownfish(Fish):
         print(self.name, self.weight, self.color)
 
 
+class Tang(Fish):
+    def __init__(self, name, weight, color, memoryloss):
+        super(Tang, self).__init__(name, weight, color)
+        self.memoryloss = memoryloss
+
+    def feed(self):
+        self.weight = self.weight + 1
+
+    def status(self):
+        print(self.name, self.weight, self.color)
+
+
+class Kong(Fish):
+    def __init__(self, name, weight, color):
+        super(Kong, self).__init__(name, weight, color)
+
+    def feed(self):
+        self.weight = self.weight + 2
+
+    def status(self):
+        print(self.name, self.weight, self.color)
+
 
 fishTank1 = Aquarium()
 fish1 = Clownfish("Nemo", 10, "orange", "white")
-fishTank1.addFish(fish1)
+fishTank1.addfish(fish1)
 fish1.status()
 fishTank1.feed()
-fishTank1.getStatus()
+fishTank1.getstatus()
